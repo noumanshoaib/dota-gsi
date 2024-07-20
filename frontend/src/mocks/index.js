61c89b1bad0a3b3d9dad9a447c2492836e6255cf
noumanshoaib-asharq <nouman.shoaib786@gmail.com>
2024-07-20 14:49:14 +0400

    updated readme

--- frontend/src/mocks/index.js
+++ frontend/src/mocks/index.js
@@ -1,16 +1,16 @@
 const mockData = {
-  "activeTeam": "dire",
-  "pick": true,
-  "activeTeamTimeRemaining": "0:21",
+  "activeTeam": 0,
+  "pick": false,
+  "activeTeamTimeRemaining": "0:00",
   "team2": {
     "teamName": "radiant",
-    "bonusTime": "1:36",
+    "bonusTime": "0:00",
     "homeTeam": false,
     "picks": [
       {
-        "id": 88,
-        "class": "nyx_assassin",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/nyx_assassin_full.png",
+        "id": 0,
+        "class": "",
+        "image": "",
         "pickedBy": {}
       },
       {
@@ -40,24 +40,24 @@ const mockData = {
     ],
     "bans": [
       {
-        "id": 86,
-        "class": "rubick",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/rubick_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 114,
-        "class": "monkey_king",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/monkey_king_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 128,
-        "class": "snapfire",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/snapfire_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 129,
-        "class": "mars",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/mars_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
         "id": 0,
@@ -78,13 +78,13 @@ const mockData = {
   },
   "team3": {
     "teamName": "dire",
-    "bonusTime": "1:40",
-    "home_team": true,
+    "bonusTime": "0:00",
+    "homeTeam": false,
     "picks": [
       {
-        "id": 16,
-        "class": "sand_king",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/sand_king_full.png",
+        "id": 0,
+        "class": "",
+        "image": "",
         "pickedBy": {}
       },
       {
@@ -114,29 +114,29 @@ const mockData = {
     ],
     "bans": [
       {
-        "id": 123,
-        "class": "hoodwink",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/hoodwink_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 11,
-        "class": "nevermore",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/nevermore_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 74,
-        "class": "invoker",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/invoker_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 10,
-        "class": "morphling",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/morphling_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
-        "id": 106,
-        "class": "ember_spirit",
-        "image": "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/ember_spirit_full.png"
+        "id": 0,
+        "class": "",
+        "image": ""
       },
       {
         "id": 0,
